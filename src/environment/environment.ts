b0a0502a304f45acc80f5232cbabee21d7beeef3
soul <soulredouane@gmail.com>
2024-01-08 18:10:51 +0100

    changing docker-compose and environment files

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment={
   production:false,
   API_BASE_URL: 'http://localhost:8080'
+  // API_BASE_URL: import.meta.env.BACKEND_URL,
+  // API_BASE_URL:"http://backend:8080",
 }
